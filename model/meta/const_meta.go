ea4ec269d6faba51b94cf6a5476bab28ae5df950
lishaohui <lishaohui@yy.com>
2020-08-10 15:30:47 +0800

    doc

--- model/meta/const_meta.go
+++ model/meta/const_meta.go
@@ -1,6 +1,6 @@
 package meta
 
-//ObjectKind 对象定义唯一标识，eg user/role
+//ObjectKind 对象定义唯一标识，eg user/role, 用于ID生成器唯一Key等
 type ObjectKind string
 
 //APIVersion 对象api版本，拓展信息，不兼容的api改动时可标识 eg api/v1  api/v2
