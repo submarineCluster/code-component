5e9ed85624351d83e4bec0735cf90feb947728bd
johanjingli <johanjingli@tencent.com>
2021-08-10 17:36:14 +0800

    feat: fix db lib --story=866492647

--- model/meta/object_meta.go
+++ model/meta/object_meta.go
@@ -1,5 +1,7 @@
 package meta
 
+import "time"
+
 //ObjectMeta 对象 metadata
 type ObjectMeta struct {
 
@@ -13,13 +15,13 @@ type ObjectMeta struct {
 	Title string `json:"title" db:"title" bson:"title" gorm:"column:title"`
 
 	// CreateTimestamp 资源创建时间戳
-	CreateTimestamp int64 `json:"createTimestamp" db:"create_timestamp" bson:"create_timestamp" gorm:"column:create_timestamp"`
+	CreateTimestamp time.Time `json:"createTimestamp" db:"create_timestamp" bson:"create_timestamp" gorm:"column:create_timestamp"`
 
 	// Creator 创建人Name
 	Creator string `json:"creator" db:"creator" bson:"creator" gorm:"column:creator"`
 
 	// ModifyTimeStamp 资源更新时间戳
-	ModifyTimestamp int64 `json:"modifyTimestamp" db:"modify_timestamp" bson:"modify_timestamp"gorm:"column:modify_timestamp"`
+	ModifyTimestamp time.Time `json:"modifyTimestamp" db:"modify_timestamp" bson:"modify_timestamp"gorm:"column:modify_timestamp"`
 
 	// Modifier 更新人Name
 	Modifier string `json:"modifier" db:"modifier" bson:"modifier" gorm:"column:modifier"`
@@ -28,7 +30,7 @@ type ObjectMeta struct {
 	Comment string `json:"comment" db:"comment" bson:"comment" gorm:"column:comment"`
 
 	// DeleteTimestamp 删除时间，零值标识未删除
-	DeleteTimestamp int64 `json:"deleteTimestamp" db:"delete_timestamp" bson:"delete_timestamp" gorm:"column:delete_timestamp"`
+	DeleteTimestamp time.Time `json:"deleteTimestamp" db:"delete_timestamp" bson:"delete_timestamp" gorm:"column:delete_timestamp"`
 
 	// Namespace 资源隔离标识 默认隔离业务ID appID 例如 {"businessCode":["5608","6666"]} 标识对象所属空间 用户拥有5608 6666的空间
 	Namespace Namespace `json:"namespace" db:"namespace" bson:"namespace" gorm:"column:namespace"`
