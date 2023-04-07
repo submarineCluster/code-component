07bf8f266b7f3ff2afac593eae9db6eb7d6d7f16
corrinzhang <corrinzhang@tencent.com>
2023-04-07 19:54:36 +0800

    modify DBModelFilter

--- model/option/list_option.go
+++ model/option/list_option.go
@@ -241,7 +241,7 @@ func DBModelFilter(dbModel *gorm.DB, option *ListOption) (*gorm.DB, error) {
 				dbModel = dbModel.Where(fmt.Sprintf(`%v <= %v and %v <= %v`, valueList[0], key, key, valueList[1]))
 			default:
 				//dbModel = dbModel.Where(fmt.Sprintf(`%v %v %v`, key, operator(value.Operator), value.Value))
-				dbModel = dbModel.Where("? ? ?", key, operator(value.Operator), value.Value)
+				dbModel = dbModel.Where(fmt.Sprintf(`%v %v ?`, key, operator(value.Operator)), value.Value)
 			}
 		}
 	}
